9f57f028fb016202edb624ce85a20790104a4fc2
Ana Luna <luna.ana92@gmail.com>
2020-08-25 02:06:55 -0700

    adding functions

--- assets/script.js
+++ assets/script.js
@@ -2,5 +2,12 @@
 document.addEventListener('DOMContentLoaded', function () {
     var elems = document.querySelectorAll('.dropdown-trigger');
     var instances = M.Dropdown.init(elems, dropdown1);
-})
+});
+
+
+// Carousel function:
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.carousel-slider');
+    M.Carousel.init(elems, {});
+  });
 
