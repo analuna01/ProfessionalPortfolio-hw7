c027dbd1d69d78a5b4668cff9fddff50a9949025
Ana Luna <luna.ana92@gmail.com>
2020-08-25 18:13:47 -0700

    Fixed footer

--- assets/script.js
+++ assets/script.js
@@ -9,5 +9,9 @@ document.addEventListener('DOMContentLoaded', function () {
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.carousel-slider');
     M.Carousel.init(elems, {});
+  })
+  var instance = M.Carousel.init({
+    fullWidth: true,
+    indicators: true
   });
 
